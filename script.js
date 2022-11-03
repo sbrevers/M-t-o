1a64e280bdd4b3bd64b00f0cbbf5031fbd702e04
sbrevers <bsp001@hotmail.com>
2022-11-03 15:01:05 +0100

    Tentative

--- script.js
+++ script.js
@@ -11,6 +11,40 @@ const template = document;
 const weatherUrl = `api.openweathermap.org/data/2.5/forecast?units=metric&lat=${liege.latitude}&lon=${liege.longitude}&appid=${apiKey}&cnt=${counter}&lang=${langage}&units=metric`;
 console.log(weatherUrl);
 
-fetch(weatherUrl)
+/*fetch(weatherUrl)
   .then((response) => response.json())
-  .then((response) => console.log(response));
+  .then((response) => console.log(response));*/
+
+function fetchingDatas() {
+  return fetch(weatherUrl).then((response) => {
+    if (response.status >= 200 && response.status < 300) {
+      return response.json();
+    } else {
+      return response.json().then((error) => {
+        console.log(error);
+        throw new Error("Something went wrong - server-side");
+      });
+    }
+  });
+}
+
+async function displayDatas() {
+  const calls = (await fetchingDatas()) || [];
+  const callList = calls.list;
+  callList.forEach((call) => {
+    const templateElement = document.importNode(
+      document.querySelector("template").content,
+      true
+    );
+    templateElement.getElementById("date").textContent = call.dt_txt;
+    // templateElement.getElementById("icon").textContent = call.weather[0].icon;
+    templateElement.getElementById("maxTemp").textContent = call.main.temp_max;
+    templateElement.getElementById("minTemp").textContent = call.main.temp_min;
+    templateElement.getElementById("windSpeed").textContent = call.wind.speed;
+    templateElement.getElementById("description").textContent =
+      call.weather[0].description;
+
+    document.querySelector("main").appendChild(templateElement);
+  });
+}
+displayDatas();
